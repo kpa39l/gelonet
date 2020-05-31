38df144c3b17056cb1faa5733ff96617512659cf
Kpa39l <kpa39l@yandex.ru>
2020-05-31 19:07:19 +0300

    import pelican-plugins

--- pelicanconf.py
+++ pelicanconf.py
@@ -8,10 +8,10 @@ AUTHOR = 'Kpa39l'
 SITEURL = 'http://localhost:8000'
 SITENAME = 'Стороженко Евгений Владимирович'
 SITETITLE = 'Стороженко Евгений Владимирович'
-SITESUBTITLE = 'Субьективное ИТ в малом городе'
-SITEDESCRIPTION = 'Сайт о ИТ и ИТ-сообщетсве в малом городе'
-# SITELOGO = ''
-# FAVICON = '/images/favicon.ico'
+SITESUBTITLE = 'Субьективно обо всем вокруг'
+SITEDESCRIPTION = 'Взгляд ИТ-шника из маленького городка на происходящее'
+SITELOGO = 'images/avatar1.jpg'
+FAVICON = 'images/favicon.ico'
 BROWSER_COLOR = '#333333'
 USE_LESS = True
 PYGMENTS_STYLE = 'monokai'
@@ -19,8 +19,17 @@ TIMEZONE = 'Europe/Moscow'
 
 ROBOTS = 'index, follow'
 THEME = "/home/kpa39l/pelican/projects/gelonet.ru/themes/Flex/"
+
 PATH = 'content'
 OUTPUT_PATH = 'output/'
+ARTICLE_PATHS = ['blog', 'news']
+ARTICLE_SAVE_AS = '{date:%Y}/{date:%m}/{date:%d}/{slug}.html'
+ARTICLE_URL = '{date:%Y}/{date:%m}/{date:%d}/{slug}.html'
+PAGE_PATHS = ['pages']
+STATIC_PATHS = ['images', 'content']
+
+PLUGIN_PATHS = ['./pelican-plugins']
+PLUGINS = ['assets', 'sitemap', 'summary']
 
 DATE_FORMATS = {
     'en': '%B %d, %Y',
@@ -40,10 +49,9 @@ HOME_HIDE_TAGS = True
 
 # Social widget
 SOCIAL = (
-    ('github', 'https://github.com/kpa39l'),
-
-    ('facebook', 'https://facebook.com/kpa39l'),
     ('vk', 'https://vk.com/kpa39l'),
+    ('github', 'https://github.com/kpa39l'),
+    ('facebook', 'https://facebook.com/kpa39l'),
     ('twitter', 'https://twitter.com/kpa39l'),
     ('youtube', 'https://www.youtube.com/channel/UC0qtXmhsIHlguW6OHZEy0Tg?view_as=subscriber'),
     ('rss', '/blog/feeds/all.atom.xml'),
